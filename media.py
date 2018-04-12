639c1fcabf254a69ffdabbb398440afc95b37c5f
Gumingyu <mrgumingyu@gmail.com>
2018-04-12 10:38:11 +0800

    Update media.py

--- media.py
+++ media.py
@@ -4,7 +4,9 @@ import webbrowser
 class Movie():
 
     """This class provides a way to store movie related information.
-    The Movie class holds __init__ and show_trailer attributes in this file."""
+    It allows us to call out the information of many different movies.
+    The Movie class holds the fllowing attributes: the title, storyline,
+    poster image and trailer of the movie. The data type is string."""
 
     def __init__(self, movie_title, movie_storyline,
                  poster_image, trailer_youtube):
